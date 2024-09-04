38302dfb5f310d89005a8ff25be78ce150819cc5
tromb <trombecher@icloud.com>
2024-09-04 02:58:44 +0200

    Improve perf, add art teaser, add raw blog post

---
+++ src/content/blog/how-to-make-visuals-like-this.md
@@ -0,0 +1,6 @@
+---
+title: "How To Make Visuals Like This ↑"
+pubUnix: 0
+---
+
+This blog post is still under construction...
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,14 @@
-/*
-import { defineCollection, z } from 'astro:content';
-
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-*/
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
+    blog: defineCollection({
+        type: "content",
+        schema: z.object({
+            title: z.string(),
+            pubUnix: z.coerce.number(),
+		    updatedUnix: z.coerce.number().optional(),
+            tags: z.array(z.string()).optional(),
+            hero: z.string().optional()
+        })
+    })
 };
