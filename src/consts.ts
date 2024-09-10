598bdaa10020324e7e4c82a2e3bad8a72239bf4f
Trombecher <trombecher@icloud.com>
2024-09-10 23:19:46 +0200

    Accurate colors

--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { getCollection } from "astro:content";
+
 export const isLowPowerMode = () => {
     /*
     if (navigator.getBattery) {
@@ -14,3 +16,22 @@ export const isLowPowerMode = () => {
 export const isDarkModeQuery = () => matchMedia("(prefers-color-scheme: dark)");
 
 export const isDarkMode = (q: {matches: boolean} = isDarkModeQuery()) => q.matches;
+
+export const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+export const NUMBERS = "0123456789";
+export const SYMBOLS = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+export const generatePassword = (length: number, charset: string) => {
+    if(charset === "") return "";
+    
+    let s = "";
+    while(length--)
+        s += charset[Math.floor(Math.random() * charset.length)];
+    return s;
+};
+
+export const escapeRegExp = (text: string) => text.replaceAll(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+
+export const getAllPosts = async () => (await getCollection("posts"))
+    .filter(e => import.meta.env.DEV || e.data.pubUnix !== undefined);
