702a81bfb90990bca096c26a531036b59c9dc19a
tromb <trombecher@icloud.com>
2024-09-22 14:20:24 +0200

    Fix a few bugs

--- src/server-util.ts
+++ src/server-util.ts
@@ -3,7 +3,7 @@ import { getCollection } from "astro:content";
 export const getAllPosts = async (
     strip: boolean = !import.meta.env.DEV,
     onlyWithTag?: string
-) => (await getCollection("posts")).filter(post => (!strip || post.data.pubUnix !== undefined)
+) => (await getCollection("posts")).filter(post => !(strip && post.data.pubUnix === undefined)
         && (!onlyWithTag || (post.data.tags as readonly string[]).includes(onlyWithTag)));
 
 export const TAGS = ["tools", "articles"] as const;
--- src/util.ts
+++ src/util.ts
@@ -34,7 +34,6 @@ export const generatePassword = (length: number, charset: string) => {
 
 export const escapeRegExp = (text: string) => text.replaceAll(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 
-
 const FORMAT_UNITS = [
     ["year", 365 * 24 * 60 * 60],
     ["month", 30 * 24 * 60 * 60],
@@ -60,3 +59,70 @@ export const formatDuration = (seconds: number) => {
     }
     return "no time";
 }
+
+const encodeUTF16LE = (str: string) => {
+    const buf = new ArrayBuffer(str.length * 2);
+    const bufView = new Uint16Array(buf);
+
+    for(let i = 0; i < str.length; ++i)
+        bufView[i] = str.charCodeAt(i);
+
+    return new Uint8Array(buf);
+};
+
+const encodeUTF16BE = (str: string) => {
+    const buf = new ArrayBuffer(str.length * 2);
+    const bufView = new Uint8Array(buf);
+
+    for(let i = 0; i < str.length; ++i) {
+        const code = str.charCodeAt(i);
+        bufView[i * 2] = code >> 8;
+        bufView[i * 2 + 1] = code;
+    }
+
+    return bufView;
+};
+
+const encodeUTF32LE = (str: string) => {
+    const buf = new ArrayBuffer(str.length * 4);
+    const bufView = new Uint32Array(buf);
+    for(let i = 0; i < str.length; ++i)
+        bufView[i] = str.charCodeAt(i);
+    return new Uint8Array(buf);
+};
+
+const encodeUTF32BE = (str: string) => {
+    const buf = new ArrayBuffer(str.length * 4);
+    const bufView = new Uint8Array(buf);
+
+    for(let i = 0; i < str.length; ++i) {
+        const realI = i * 4;
+
+        const code = str.charCodeAt(i);
+        bufView[realI] = code >> 24;
+        bufView[realI + 1] = code >> 16;
+        bufView[realI + 2] = code >> 8;
+        bufView[realI + 3] = code;
+    }
+
+    return bufView;
+}
+
+const bufferToStrings = (buf: Uint8Array) => Array.from(buf, byte => byte.toString(16).padStart(2, "0"));
+
+const textEncoder = new TextEncoder();
+
+export const SUPPORTED_TEXT_ENCODINGS: [string, (str: string) => string[]][] = [
+    ["Unicode Code Points", str => str.split("").map(c => `U+${c.charCodeAt(0)!.toString(16).padStart(4, "0")}`)],
+    ["ASCII", str => {
+        const encoded = textEncoder.encode(str);
+        if(encoded.findIndex(byte => byte > 127) !== -1)
+            return ["Input cannot be encoded with ASCII."]
+        else return bufferToStrings(encoded);
+    }],
+    ["UTF-8", str => bufferToStrings(textEncoder.encode(str))],
+    ["UTF-16LE", str => bufferToStrings(encodeUTF16LE(str))],
+    ["UTF-16BE", str => bufferToStrings(encodeUTF16BE(str))],
+    ["UTF-32LE", str => bufferToStrings(encodeUTF32LE(str))],
+    ["UTF-32BE", str => bufferToStrings(encodeUTF32BE(str))],
+];
