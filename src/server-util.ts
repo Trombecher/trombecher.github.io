784d73f51eab935976191295a70c9a79e7e2b1b1
tromb <trombecher@icloud.com>
2024-09-25 21:07:06 +0200

    Fix tags

--- src/server-util.ts
+++ src/server-util.ts
@@ -2,10 +2,32 @@ import { getCollection } from "astro:content";
 
 export const getAllPosts = async (
     strip: boolean = !import.meta.env.DEV,
-    onlyWithTag?: string
+    onlyWithTag?: Tag
 ) => (await getCollection("posts")).filter(post => !(strip && post.data.pubUnix === undefined)
         && (!onlyWithTag || (post.data.tags as readonly string[]).includes(onlyWithTag)));
 
-export const TAGS = ["tools", "articles"] as const;
+export const TAGS = ["Tool", "Article", "Project"] as const;
 
-export const toPascalCase = (s: string) => s[0]!.toUpperCase() + s.slice(1);
+export const SLUG_TO_TAG = {
+    tools: "Tool",
+    articles: "Article",
+    projects: "Project"
+} as const;
+
+export const TAG_TO_SLUG = {
+    Tool: "tools",
+    Article: "articles",
+    Project: "projects"
+} as const;
+
+export const TAG_TO_MULTIPLE = {
+    Tool: "Tools",
+    Article: "Articles",
+    Project: "Projects"
+} as const;
+
+export type Tag = typeof TAGS[number];
+export type Slug = typeof TAG_TO_SLUG[Tag];
+export type Multiple = typeof TAG_TO_MULTIPLE[Tag];
+
+// export const toPascalCase = (s: string) => s[0]!.toUpperCase() + s.slice(1);
