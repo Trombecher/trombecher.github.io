a21776f8a4667cbc975e58b16af32199e1ad0d94
Tobias Hillemanns <trombecher@icloud.com>
2024-12-06 06:47:22 +0100

    This one?

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     vite: {
         resolve: {
             alias: {
-                '@': '/src',
+                "@": "./src",
             },
         },
     },
